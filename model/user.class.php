47ff72722143fe25f00e57f1083afd61eb85d940
hud <hud@recom3.com>
2023-10-04 12:30:54 +0200

    user model

--- model/user.class.php
+++ model/user.class.php
@@ -1,15 +1,54 @@
 <?php
 
 /**
- * Model for user.
+ * user short summary.
  *
- * This file hold the user class.
+ * user description.
  *
  * @version 1.0
  * @author recom3
  */
+class UserProfileClass implements JsonSerializable
+{
+    public $id;
+    public $auto_upload_trips;
+    public $phone_number;
+    public $gender;
+    public $city;
+    public $country_id;
+    public $bio;
+
+    public function jsonSerialize() {
+        return (object) get_object_vars($this);
+    }
+}
+
+class UserClass implements JsonSerializable
+{
+    public $id;
+    public $email;
+    public $first_name;
+    public $last_name;
+    public $facebook_id;
+    public $mobile_active;
+    public $last_login;
+    public $measurement;
+    public $buddy_tracking_enabled;
+    public $buddy_tracking_stealth_mode_enabled;
+
+    public function jsonSerialize() {
+        return (object) get_object_vars($this);
+    }
+}
+
 class UserData implements JsonSerializable
 {
+    public $access_token;
+    public $token_type;
+    public $expires;
+    public $refresh_token;
+    public $User;
+    public $UserProfile;
 
     public function jsonSerialize() {
         return (object) get_object_vars($this);
